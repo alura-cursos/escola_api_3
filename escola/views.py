b2a996fcaa2cc93ec45f630612f943afcd55642d
Guilherme Lima <guilhermelimaruby@gmail.com>
2020-11-17 16:11:37 -0300

    aula_4

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, generics
+from rest_framework import status
 from escola.models import Aluno, Curso, Matricula
 from escola.serializer import AlunoSerializer, AlunoSerializerV2, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
+from rest_framework.response import Response
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
@@ -15,6 +17,15 @@ class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(serializer.data, status=status.HTTP_201_CREATED)
+            id = str(serializer.data['id'])
+            response['Location'] = request.build_absolute_uri() + id
+            return response
 
 class MatriculaViewSet(viewsets.ModelViewSet):
     """Listando todas as matrículas"""
